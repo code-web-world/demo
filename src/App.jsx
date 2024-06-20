8c5dd2470de84281d038f961937a822646199162
CodeWebWorld/ <158747587+code-web-world@users.noreply.github.com>
2024-06-20 07:33:57 +0000

    yez

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import myImage from './assets/2.svg';
+import myImage2 from './assets/img.jpg';
 export default function App() {
   return (
     <main>
       React ⚛️ + Vite ⚡ + Replit
       <img src={myImage}/>
+      <img src={myImage2}/>
       
     </main>
   )
